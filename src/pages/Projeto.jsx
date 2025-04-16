40d3dd6eb711e63344b117329f6e8634bbc83d99
Alessandro <aleluinisilva@gmail.com>
2025-04-16 02:48:05 -0300

    pagina do projeto/ jogo

---
+++ src/pages/Projeto.jsx
@@ -0,0 +1,10 @@
+import BreakoutGame from "../components/jogoB/BreakoutGame"
+
+export const Projeto = () => {
+    return (
+        <section>
+                <h1>PAGINA PROJETO</h1>
+                <BreakoutGame />
+        </section>
+    )
+}
