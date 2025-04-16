7f780af9c8c15e45252a5df98275c1010c614a0e
Alessandro <aleluinisilva@gmail.com>
2025-04-16 02:54:12 -0300

    pagina de contatos

---
+++ src/pages/contato.jsx
@@ -0,0 +1,14 @@
+export const Contato = () => {
+    return (
+        <section>
+                <h1>contados dos devs</h1>
+                <h2>emails</h2>
+                <p>alessandro_coutinho@estudante.sesisenai.org.br</p>
+                <p>joao_c_formento@estudante.sesisenai.org.br</p>
+                <h2>GitHub</h2>
+                <a href="https://github.com/Alessandrovlr">Alessandro</a>
+                <br />
+                <a href="https://github.com/JoaoCarlosForment0">João</a>
+        </section>
+    )
+}
