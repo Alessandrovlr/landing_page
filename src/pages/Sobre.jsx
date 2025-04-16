9797469636bc80b65a7b21d6dc4aeee8a7fa928e
Alessandro <aleluinisilva@gmail.com>
2025-04-16 02:53:53 -0300

    pagina sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,7 +1,23 @@
-export const About = () => {
+export const Sobre = () => {
     return (
         <section>
-                <h1>About</h1>
+                <h1>PAGINA SOBRE</h1>
+                <p>Desenvolvido por Alessandro luini da silva coutinho e João Carlos Formento</p>
+                <h2>Por que criaramos o projeto</h2>
+                <p>
+                    Este projeto foi desenvolvido com o objetivo de recriar o clássico jogo Breakout, utilizando React, juntamente com JavaScript, HTML5 e CSS. Breakout é um jogo lançado nos anos 70, onde o jogador controla uma plataforma horizontal que deve rebater uma bola, com o objetivo de destruir todos os blocos presentes na tela e acumular pontos.
+                    A recriação deste jogo tem como propósito explorar e aplicar conceitos fundamentais de programação, manipulação de elementos gráficos via Canvas, controle de eventos do teclado, lógica de colisão, além de reforçar boas práticas com classes e estruturação de código em JavaScript.
+                </p>
+                <h2>Tecnologias usadas</h2>
+                <p>
+                    <ul>
+                        <li>REACT</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JAVASCRIPT</li>
+                        <li>TAILWIND</li>
+                    </ul>
+                </p>
         </section>
     )
 }
