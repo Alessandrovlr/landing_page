4603fbaff73f346e4b9236eeca544dd30a770530
ALESSANDRO LUINI DA SILVA COUTINHO <alessandro_coutinho@estudante.sesisenai.org.br>
2025-04-23 16:47:37 -0300

    Mudanças no hover e cores

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Menu = () => {
   const estiloLinks =
-    "no-underline text-[20px] text-[aliceblue]  mx-[10px] hover:text-[22px] transform duration-200";
+    "no-underline text-[20px] text-[aliceblue]  mx-[10px] hover:text-[rgb(136,83,221)] transform duration-200";
   return (
-    <nav className="flex h-[7%] items-center gap-[70px] pl-[50px] bg-black">
+    <nav className="flex h-[8%] items-center gap-[70px] pl-[50px] bg-black">
       <Link to="/" className={estiloLinks}>Home</Link>
-      <Link to="/Projeto" className={estiloLinks}>Projeto</Link>
+      <Link to="/projeto" className={estiloLinks}>Projeto</Link>
       <Link to="/sobre" className={estiloLinks}>Sobre</Link>
       <Link to="/contato" className={estiloLinks}>Contato</Link>
     </nav>
