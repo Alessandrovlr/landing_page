dfdd851918fe6dc21362a6d05313dbaffd500750
João Carlos Formento <jcformento13@gmail.com>
2025-04-23 09:52:41 -0300

    Estilização do Menu(Nav) pronta

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom"
-import './menu.css'
+import { Link } from "react-router-dom";
 
 export const Menu = () => {
-    return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/Projeto'>Projeto</Link>
-            <Link to='/sobre'>Sobre</Link>
-            <Link to='/contato'>Contato</Link>
-        </nav>
-    )
-}
+  const estiloLinks =
+    "no-underline text-[20px] text-[aliceblue]  mx-[10px] hover:text-[22px] transform duration-200";
+  return (
+    <nav className="flex items-center gap-[70px] pl-[50px] bg-black">
+      <Link to="/" className={estiloLinks}>Home</Link>
+      <Link to="/Projeto" className={estiloLinks}>Projeto</Link>
+      <Link to="/sobre" className={estiloLinks}>Sobre</Link>
+      <Link to="/contato" className={estiloLinks}>Contato</Link>
+    </nav>
+  );
+};
