9c8035b56488cd88e0ade493d65c6bbbc5c72d31
JOÃO CARLOS FORMENTO <joao_c_formento@estudante.sesisenai.org.br>
2025-04-23 16:56:14 -0300

    Adição da feature dark/light mode

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import { BotaoDarkMode } from "../botaoDarkMode/botaoDarkMode";
 
 export const Menu = () => {
   const estiloLinks =
     "no-underline text-[20px] text-[aliceblue]  mx-[10px] hover:text-[22px] transform duration-200";
   return (
-    <nav className="flex h-[7%] items-center gap-[70px] pl-[50px] bg-black">
-      <Link to="/" className={estiloLinks}>Home</Link>
-      <Link to="/Projeto" className={estiloLinks}>Projeto</Link>
-      <Link to="/sobre" className={estiloLinks}>Sobre</Link>
-      <Link to="/contato" className={estiloLinks}>Contato</Link>
+    <nav className="flex h-[7%] justify-between items-center pl-[50px] bg-black">
+      <div className="flex gap-[70px]">
+        <Link to="/" className={estiloLinks}>
+          Home
+        </Link>
+        <Link to="/Projeto" className={estiloLinks}>
+          Projeto
+        </Link>
+        <Link to="/sobre" className={estiloLinks}>
+          Sobre
+        </Link>
+        <Link to="/contato" className={estiloLinks}>
+          Contato
+        </Link>
+      </div>
+      <BotaoDarkMode></BotaoDarkMode>
     </nav>
   );
 };
