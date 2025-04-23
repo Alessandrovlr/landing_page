002d89be4b019be4405d3aaa2c552ebfb76dc33d
João Carlos Formento <jcformento13@gmail.com>
2025-04-23 09:42:02 -0300

    Estilização da pagina Projeto pronta

--- src/pages/Projeto.jsx
+++ src/pages/Projeto.jsx
@@ -1,11 +1,9 @@
-import BreakoutGame from "../components/jogoB/BreakoutGame"
-
-import './Projeto.css';
+import BreakoutGame from "../components/jogoB/BreakoutGame";
 
 export const Projeto = () => {
-    return (
-        <section className="section-projeto">
-                <BreakoutGame />
-        </section>
-    )
-}
+  return (
+    <section className=" flex items-center justify-center pt-[1%] bg-[rgb(49,49,49)] w-[60%]">
+      <BreakoutGame />
+    </section>
+  );
+};
